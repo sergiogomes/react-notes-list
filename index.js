415fa9ab6d1e2554858bd3025739bcb05e3ed581
Sérgio Pow Gomes <sergiopowgomes@MacBook-Air-de-Sergio.local>
2018-10-05 04:15:31 -0300

    React Note
    Creating notes dynamically now. A mixin directive for box-shadow was imported. Plugin transform class properties added to babel.

--- index.js
+++ index.js
@@ -3,7 +3,68 @@ import ReactDOM from "react-dom";
 
 import "./index.scss";
 
-ReactDOM.render(
-    <h1>Bem Vindo!</h1>,
-    document.getElementById("root")
+class App extends React.Component {
+  state = {
+    notes: []
+  };
+
+  handleAddNote = text => {
+    this.setState(prevState => ({
+      notes: prevState.notes.concat(text)
+    }));
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <NewNote onAddNote={this.handleAddNote} />
+        <NoteList notes={this.state.notes} />
+      </div>
+    );
+  }
+}
+
+class NewNote extends React.Component {
+  state = {
+    text: ""
+  };
+
+  render() {
+    const { onAddNote } = this.props;
+    const { text } = this.state;
+
+    return (
+      <div className="new-note">
+        <input
+          type="text"
+          className="new-note__input"
+          placeholder="Type your note here..."
+          value={text}
+          onChange={event => {
+            this.setState({
+              text: event.target.value
+            });
+          }}
+          onKeyPress={event => {
+            if (event.key === "Enter") {
+              onAddNote(event.target.value);
+              this.setState({
+                text: ""
+              });
+            }
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+const NoteList = ({ notes }) => (
+  <div className="note-list">
+    {notes.map(note => (
+      <div className="note">{note}</div>
+    ))}
+  </div>
 );
+
+ReactDOM.render(<App />, document.getElementById("root"));
