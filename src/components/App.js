b66730ea1ec81d771c0c6ec2bd17dc8614b81c56
Sérgio Pow Gomes <sergiopowgomes@MacBook-Air-de-Sergio.local>
2018-10-09 01:12:07 -0300

    Handling Errors
    A new component was created to display a error while loading the data. A button was also created to display an unconnected cloud when there is a error saving data.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
 import uuid from "uuid/v1";
 
 import NewNote from "./NewNote";
 import NoteList from "./NoteList";
 import AppBar from "./AppBar";
+import NoteService from "../Services/NoteServices";
+import Error from "./Error";
 
 class App extends React.Component {
   state = {
     notes: [],
-    isLoading: false
+    isLoading: false,
+    reloadHasError: false,
+    saveHasError: false
   };
+
+  componentDidCatch() {
+    this.setState({ reloadHasError: true });
+  }
 
   componentDidMount() {
     this.handleReload();
@@ -73,35 +81,53 @@ class App extends React.Component {
   };
 
   handleReload = () => {
-    this.setState({ isLoading: true });
-    const notes = window.localStorage.getItem("notes");
-    setTimeout(() => {
-      this.setState({ notes: JSON.parse(notes), isLoading: false });
-    }, 3000);
+    this.setState({ isLoading: true, reloadHasError: false });
+    NoteService.load()
+      .then(notes => {
+        this.setState({ notes, isLoading: false });
+      })
+      .catch(() => {
+        this.setState({ isLoading: false, reloadHasError: true });
+      });
   };
 
   handleSave = notes => {
-    this.setState({ isLoading: true });
-    setTimeout(() => {
-      window.localStorage.setItem("notes", JSON.stringify(notes));
-      this.setState({ isLoading: false });
-    }, 3000);
+    this.setState({ isLoading: true, saveHasError: false });
+    NoteService.save(notes)
+      .then(() => {
+        this.setState({ isLoading: false });
+      })
+      .catch(() => {
+        this.setState({ isLoading: false, saveHasError: true });
+      });
   };
 
   render() {
-    const { isLoading } = this.state;
+    const { notes, isLoading, reloadHasError, saveHasError } = this.state;
 
     return (
       <div>
-        <AppBar isLoading={isLoading} />
+        <AppBar
+          isLoading={isLoading}
+          saveHasError={saveHasError}
+          onSaveRetry={() => {
+            this.handleSave(notes);
+          }}
+        />
         <div className="container">
-          <NewNote onAddNote={this.handleAddNote} />
-          <NoteList
-            notes={this.state.notes}
-            onMove={this.handleMove}
-            onDelete={this.handleDelete}
-            onEdit={this.handleEdit}
-          />
+          {reloadHasError ? (
+            <Error onRetry={this.handleReload} />
+          ) : (
+            <Fragment>
+              <NewNote onAddNote={this.handleAddNote} />
+              <NoteList
+                notes={notes}
+                onMove={this.handleMove}
+                onDelete={this.handleDelete}
+                onEdit={this.handleEdit}
+              />
+            </Fragment>
+          )}
         </div>
       </div>
     );
