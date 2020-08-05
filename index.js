eab6d0d31a720fc6cb0317620fca8b475c5608d9
sergiogomes <sergiopgomes92@gmail.com>
2020-08-04 21:35:27 -0300

    Recriating components.

--- index.js
+++ index.js
@@ -1,24 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import classNames from "classnames";
+import { v1 as uuidv1 } from "uuid";
 
 import "./index.scss";
 
 class App extends React.Component {
   state = {
-    notes: ["Teste 1", "Teste 2"],
+    notes: [],
   };
 
   handleAddNote = (text) => {
     this.setState((prevState) => ({
-      notes: prevState.notes.concat(text),
+      notes: prevState.notes.concat({ id: uuidv1(), text }),
     }));
+  };
+
+  handleMove = (direction, index) => {
+    this.setState((prevState) => {
+      const newNotes = prevState.notes.slice();
+      const removedNote = newNotes.splice(index, 1)[0];
+
+      if (direction === "up") {
+        newNotes.splice(index - 1, 0, removedNote);
+      } else {
+        newNotes.splice(index + 1, 0, removedNote);
+      }
+
+      return {
+        notes: newNotes,
+      };
+    });
+  };
+
+  handleDelete = (id) => {
+    this.setState((prevState) => {
+      const newNotes = prevState.notes.slice();
+      const index = newNotes.findIndex((note) => note.id === id);
+      newNotes.splice(index, 1);
+
+      return {
+        notes: newNotes,
+      };
+    });
   };
 
   render() {
     return (
       <div className="container">
         <NewNote onAddNote={this.handleAddNote} />
-        <NoteList notes={this.state.notes} />
+        <NoteList
+          notes={this.state.notes}
+          onMove={this.handleMove}
+          onDelete={this.handleDelete}
+        />
       </div>
     );
   }
@@ -28,6 +63,7 @@ class NewNote extends React.Component {
   state = {
     text: "",
   };
+
   render() {
     const { onAddNote } = this.props;
     const { text } = this.state;
@@ -58,10 +94,40 @@ class NewNote extends React.Component {
   }
 }
 
-const NoteList = ({ notes }) => (
+const NoteList = ({ notes, onMove, onDelete }) => (
   <div className="note-list">
-    {notes.map((note) => (
-      <div className="note">{note}</div>
+    {notes.map((note, index) => (
+      <div key={note.id} className="note">
+        <span className="note__text">{note.text}</span>
+        <button
+          className="note__button"
+          onClick={() => {
+            onDelete(note.id);
+          }}
+        >
+          <i className="material-icons">delete</i>
+        </button>
+        <button
+          className={classNames("note__button", {
+            "note__button--hidden": index === 0,
+          })}
+          onClick={() => {
+            onMove("up", index);
+          }}
+        >
+          <i className="material-icons">arrow_upward</i>
+        </button>
+        <button
+          className={classNames("note__button", {
+            "note__button--hidden": index === notes.length - 1,
+          })}
+          onClick={() => {
+            onMove("down", index);
+          }}
+        >
+          <i className="material-icons">arrow_downward</i>
+        </button>
+      </div>
     ))}
   </div>
 );
